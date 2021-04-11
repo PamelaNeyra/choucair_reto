9451e68238313a8b58e74ddc36a0238695c3afa4
Pamela Neyra <elva.neyra@unmsm.edu.pe>
2021-04-10 20:15:23 -0500

    first commit

---
+++ Reto_choucair/src/test/java/RunTests.java
@@ -0,0 +1,14 @@
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        tags = "@login",
+        glue = {"stepDefinitions"},
+        plugin = {
+                "pretty"}
+)
+public class RunTests {
+}
